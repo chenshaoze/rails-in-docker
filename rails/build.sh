64176288c65dc9b18d553acfbffb0ae81af79e6e
unknown <>
2016-09-05 00:06:02 +0800

    modify rails dockerfile and build.sh

--- rails/build.sh
+++ rails/build.sh
@@ -1,11 +1,10 @@
-cp -r /tmp/code/Web-Spider /usr/src/app/
-cd /usr/src/app/
-# ls /usr/src/app/
-ruby -v
-# gem -v
-rails -v
+#!/bin/bash
+# cp -r /tmp/code/Web-Spider /usr/src/app/
+# cd `/usr/src/app/`
+echo `pwd`
+echo `ls`
 # gem install bundler
-/usr/src/app/bundle install
-rake db:create
-rake db:migrate
+`bundle install`
+# rake db:create
+# rake db:migrate
 rails s -b 0.0.0.0 -p 3000
